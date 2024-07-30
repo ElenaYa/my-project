a6dc7004da665899313d05b73f951adc45194cb5
ElenaYa <elena.yatsynyuk@gmail.com>
2024-07-30 15:58:50 +0300

    add eye icon

---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.getElementById('toggle-password').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+  });
